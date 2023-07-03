0f780491760cf9f5b4fcf341d8ea4666bd4a81ea
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2023-07-03 14:25:05 +0900

    fix: typo third_party/webhook/doc.go
    Co-authored-by: Windz <WindzCUHK@users.noreply.github.com>
    Signed-off-by: Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>

--- third_party/webhook/doc.go
+++ third_party/webhook/doc.go
@@ -1,2 +1,4 @@
-// Package log is originated from https://github.com/yahoo/k8s-athenz-webhook/releases/tag/v0.1.4
+// Package webhook is originated from https://github.com/yahoo/k8s-athenz-webhook/releases/tag/v0.1.4
+// Copyright 2019 Verizon Media Inc. Licensed under the terms of the 3-Clause BSD License.
+// See LICENSE file in github.com/yahoo/k8s-athenz-webhook/blob/v0.1.4/LICENSE for terms.
 package webhook
