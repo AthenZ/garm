7c25823958aa949a119ac4a8fa9cf82462ac3432
Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
2024-12-06 14:32:22 +0900

    feat: more debugs

--- service/cert-reloader.go
+++ service/cert-reloader.go
@@ -109,12 +109,14 @@ func (w *CertReloader) loadLocalCAPool() (*x509.CertPool, error) {
 // loadLocalCertAndKey loads cert & its key from local filesystem and update its own cache if the file has changed.
 // Used to be called as "maybeReload"
 func (w *CertReloader) loadLocalCertAndKey() error {
-	glg.Debugf("Loading .athenz.cert[%s] .athenz.key[%s] .athenz.root_ca[%s] from local file ...", w.certPath, w.keyPath, w.caPath)
+	glg.Debugf("Attempting to load .athenz.cert[%s] .athenz.key[%s] .athenz.root_ca[%s] from local file ...", w.certPath, w.keyPath, w.caPath)
 	st, err := os.Stat(w.certPath)
 	if err != nil {
 		return errors.Wrap(err, fmt.Sprintf("unable to stat %s", w.certPath))
 	}
+	// This logic will only reload the cert if the file has changed; greatly improve the performance.
 	if !st.ModTime().After(w.mtime) {
+		glg.Debugf("Skipping to load .athenz.cert[%s] .athenz.key[%s] .athenz.root_ca[%s] from local file as it has not changed ...", w.certPath, w.keyPath, w.caPath)
 		return nil
 	}
 	cert, err := tls.LoadX509KeyPair(w.certPath, w.keyPath)
